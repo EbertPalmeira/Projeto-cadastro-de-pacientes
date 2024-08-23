ca49e40eb03666732d9f979c07b4d5865c24263a
EbertPalmeira <eberttpalmerass@gmail.com>
2024-08-23 15:44:31 -0300

    alteracao

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -2,7 +2,6 @@ import {Route, BrowserRouter,Routes} from 'react-router-dom';
 import Form1 from './components/pages/Step1';
 import Form2 from './components/pages/Step2';
 import Form3 from './components/pages/Step3';
-import { useState } from 'react';
 import Form4 from './components/pages/Step4';
 import Form5 from './components/pages/Step5';
 import Home from './components/pages/Home';
